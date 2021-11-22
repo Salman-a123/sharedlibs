039d5a3f56fcb8d73b9021ca042aaa6051d94eb6
Salman-a123 <54708896+Salman-a123@users.noreply.github.com>
2021-11-22 15:28:26 +0100

    Update Helloworld.groovy

--- vars/Helloworld.groovy
+++ vars/Helloworld.groovy
@@ -1,21 +1,14 @@
-pipeline {
-  agent none
-  stages {
-    stage('Back-end') {
-      agent {
-        docker { image 'maven:3.8.1-adoptopenjdk-11' }
-      }
-      steps {
-        sh 'mvn --version'
-      }
-    }
-    stage('Front-end') {
-      agent {
-        docker { image 'node:16-alpine' }
-      }
-      steps {
-        sh 'node --version'
-      }
-    }
+def call(name){
+  echo "Hey $name. How are you...!"
+
+  node {
+     checkout scm
+     stage ("Compile") {
+	sh "gcc Example.c -o Example"
+     }
+     stage ("Run") {
+	sh "./Example"
+     }
+
   }
 }
