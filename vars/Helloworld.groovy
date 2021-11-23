da48a24256c19407937a022120c7b9c921e4af88
Salman-a123 <54708896+Salman-a123@users.noreply.github.com>
2021-11-23 11:06:59 +0100

    Update Helloworld.groovy

--- vars/Helloworld.groovy
+++ vars/Helloworld.groovy
@@ -1,25 +1,12 @@
-def call(name){
-  echo "Hey $name. How are you...!"
-  node {
-     checkout scm
-     stage ("Compile") {
-	sh "gcc hello.c -o Example"
-     }
-     stage ("Run") {
-	sh "./Example"
-     }
-	  stage("Run Docker Image") {
-	  echo "I am into the docker running stage"
-	    step {
-    		echo "I am into the check point 1"
-		docker { image 'node:16-alpine'
-		    }
-	    step {
-		echo "I am into the check point 2"
-		sh'node --version'
-	    }
-	  }
-	  }
-
-  }
+pipeline {
+    agent {
+        docker { image 'node:14-alpine' }
+    }
+    stages {
+        stage('Test') {
+            steps {
+                sh 'node --version'
+            }
+        }
+    }
 }
